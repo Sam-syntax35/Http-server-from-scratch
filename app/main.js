5c4d9df08fd0c097afaa1a3a266de2bf82a87f0e
Sam-syntax35 <mansisharma317305@gmail.com>
2026-06-30 22:06:22 +0530

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -5,7 +5,7 @@ console.log("Logs from your program will appear here!");
 
 // TODO: Uncomment the code below to pass the first stage
  const server = net.createServer((socket) => {
-   socket.write('HTTP/1.1 400 OK\r\n\r\n');
+   socket.write('HTTP/1.1 404 OK\r\n\r\n');
    socket.on("close", () => {
      socket.end();
    });
