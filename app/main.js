3e3fd89fd9938ff42dabcc44f7bb515ecef374c7
Sam-syntax35 <mansisharma317305@gmail.com>
2026-06-30 17:32:48 +0530

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -1,12 +1,15 @@
+
 const net = require("net");
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
-const server = net.createServer((socket) => {
-  socket.on("close", () => {
-    socket.end();
-  });
-});
+// TODO: Uncomment the code below to pass the first stage
+ const server = net.createServer((socket) => {
+   socket.write('HTTP/1.1 200 OK\r\n\r\n');
+   socket.on("close", () => {
+     socket.end();
+   });
+ });
 
 server.listen(4221, "localhost");
