9d13671eadc92c9e47a969ff4602405246e81ff4
Sam-syntax35 <mansisharma317305@gmail.com>
2026-06-30 18:39:05 +0530

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -1,15 +1,14 @@
-
 const net = require("net");
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
-
-// TODO: Uncomment the code below to pass the first stage
- const server = net.createServer((socket) => {
-   socket.write('HTTP/1.1 200 OK\r\n\r\n');
-   socket.on("close", () => {
-     socket.end();
-   });
- });
+const server = net.createServer((socket) => {
+  socket.on("data", (data) => {
+    const path = data.toString().split(" ")[1];
+    const responseStatus = path === "/" ? "200 OK" : "404 Not Found";
+    socket.write(`HTTP/1.1 ${responseStatus}\r\n\r\n`);
+  });
+  socket.on("close", () => {
+    socket.end();
+  });
+});
 
 server.listen(4221, "localhost");
