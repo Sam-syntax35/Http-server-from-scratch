cba7782fe3143f4d6d8e7e954beed4b3fdd07ca9
Sam-syntax35 <mansisharma317305@gmail.com>
2026-07-01 12:09:16 +0530

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -3,9 +3,26 @@ const net = require("net");
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
     const path = data.toString().split(" ")[1];
-    const responseStatus = path === "/" ? "200 OK" : "404 Not Found";
-    socket.write(`HTTP/1.1 ${responseStatus}\r\n\r\n`);
+
+    if (path === "/") {
+      socket.write("HTTP/1.1 200 OK\r\n\r\n");
+    } 
+    else if (path.startsWith("/echo/")) {
+      const message = path.substring(6); // Extract text after "/echo/"
+
+      socket.write(
+        `HTTP/1.1 200 OK\r\n` +
+        `Content-Type: text/plain\r\n` +
+        `Content-Length: ${message.length}\r\n` +
+        `\r\n` +
+        `${message}`
+      );
+    } 
+    else {
+      socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+    }
   });
+
   socket.on("close", () => {
     socket.end();
   });
