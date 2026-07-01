7fa3d20af913670ddfb7617a7e7e7be05b01b828
Sam-syntax35 <mansisharma317305@gmail.com>
2026-07-01 12:38:40 +0530

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -2,30 +2,42 @@ const net = require("net");
 
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
-    const path = data.toString().split(" ")[1];
+    const request = data.toString();
 
-    if (path === "/") {
-      socket.write("HTTP/1.1 200 OK\r\n\r\n");
-    } 
-    else if (path.startsWith("/echo/")) {
-      const message = path.substring(6); // Extract text after "/echo/"
+    // First line: GET /user-agent HTTP/1.1
+    const requestLine = request.split("\r\n")[0];
+    const path = requestLine.split(" ")[1];
+
+    if (path === "/user-agent") {
+      const lines = request.split("\r\n");
+
+      let userAgent = "";
+
+      // Find the User-Agent header
+      for (const line of lines) {
+        if (line.startsWith("User-Agent:")) {
+          userAgent = line.split(": ")[1];
+          break;
+        }
+      }
 
       socket.write(
         `HTTP/1.1 200 OK\r\n` +
         `Content-Type: text/plain\r\n` +
-        `Content-Length: ${message.length}\r\n` +
+        `Content-Length: ${userAgent.length}\r\n` +
         `\r\n` +
-        `${message}`
+        userAgent
       );
-    } 
-    else {
-      socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+    } else {
+      socket.write(
+        `HTTP/1.1 404 Not Found\r\n\r\n`
+      );
     }
-  });
 
-  socket.on("close", () => {
     socket.end();
   });
 });
 
-server.listen(4221, "localhost");
+server.listen(4221, () => {
+  console.log("Server listening on port 4221");
+});
